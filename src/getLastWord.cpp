fed8359f505c911b7569763f96786053a2cab502
Seshu1729 <Seshu1729@missionrnd.com>
2016-01-13 14:22:52 +0530

    Submit Commit

--- src/getLastWord.cpp
+++ src/getLastWord.cpp
@@ -10,7 +10,43 @@ Note:Dont modify original string Neglect Spaces at the right end and at left end
 */
 #include <stdlib.h>
 
-char * get_last_word(char * str){
+void swap(char *variable1, char *variable2)
+{
+	char temparary;
+	temparary = *variable1;
+	*variable1 = *variable2;
+	*variable2 = temparary;
+}
 
-	return NULL;
+void stringReverse(char input[], int length)
+{
+	int start = 0, end = length - 1;
+	while (start<end)
+		swap(&input[start++], &input[end--]);
+}
+
+int length(char input[])
+{
+	int length = -1;
+	while (input[++length] != '\0');
+	return length;
+}
+
+char * get_last_word(char * str)
+{
+	char *res;
+	int ind1 = length(str) - 1, ind2 = 0;
+	res = (char *)malloc(0);
+	while (str[ind1] == ' '&&ind1 != -1)
+		ind1--;
+	while (str[ind1] != ' '&&ind1 != -1)
+	{
+		ind2++;
+		res = (char *)realloc(res, ind2);
+		res[ind2 - 1] = str[ind1];
+		ind1--;
+	}
+	res[ind2] = '\0';
+	stringReverse(res, ind2);
+	return res;
 }
